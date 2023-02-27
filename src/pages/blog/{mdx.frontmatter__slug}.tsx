03c9b3655eac7fc0fa5af06f1f73f8232bb3caf6
Joohyung Park <johy@dabyeol.com>
2023-02-28 01:44:16 +0900

    Initial commit

---
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -0,0 +1,46 @@
+import { graphql } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import React from "react";
+import Layout from "../../components/Layout";
+import Seo from "../../components/Seo";
+
+interface IBlogPostProps {
+  data: Queries.PostDetailQuery;
+  children: any;
+}
+
+export default function BlogPost({ data, children }: IBlogPostProps) {
+  const image = getImage(
+    data.mdx?.frontmatter?.headerImage?.childImageSharp?.gatsbyImageData!
+  );
+
+  return (
+    <Layout title="">
+      <GatsbyImage image={image as any} alt={data.mdx?.frontmatter?.title!} />
+      <div>{children}</div>
+    </Layout>
+  );
+}
+
+export const query = graphql`
+  query PostDetail($frontmatter__slug: String) {
+    mdx(frontmatter: { slug: { eq: $frontmatter__slug } }) {
+      frontmatter {
+        author
+        category
+        date
+        slug
+        title
+        headerImage {
+          childImageSharp {
+            gatsbyImageData(height: 450, placeholder: BLURRED)
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const Head = ({ data }: IBlogPostProps) => (
+  <Seo title={data.mdx?.frontmatter?.title!} />
+);
